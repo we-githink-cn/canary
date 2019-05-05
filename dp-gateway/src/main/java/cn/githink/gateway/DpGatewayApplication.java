fd480b01228094e8f11dd6f754b8353830dd6389
Maozk <549595297@qq.com>
2019-05-05 22:37:07 +0800

    :mute:删除日志记录

---
+++ dp-gateway/src/main/java/cn/githink/gateway/DpGatewayApplication.java
@@ -0,0 +1,14 @@
+package cn.githink.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class DpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpGatewayApplication.class, args);
+    }
+
+}
---
+++ dp-gateway/src/test/java/cn/githink/gateway/DpGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.githink.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DpGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
